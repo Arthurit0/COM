062573a8d7b6bbe99d9a43faa7f392cea4b5c7b8
Arthur José <arthur.jose27@gmail.com>
2023-04-04 08:40:28 -0300

    Tentando fazer Árvore Binária em Haskell

---
+++ Binary_Tree_Haskell/tree.hs
@@ -0,0 +1,9 @@
+data Arvore a = No a (Arvore a) (Arvore a) | Folha
+ins e Folha = No e Folha Folha
+ins e (No x esq dir) | e == x = No x esq dir
+                     | e < x = No x (ins e esq) dir 
+                     | otherwise = No x esq (ins e dir)
+
+emOrdem :: Arvore a -> [a]
+-- emOrdem Folha = 
+-- emOrdem (No x esq dir) =
