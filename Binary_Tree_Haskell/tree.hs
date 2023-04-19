a0445a81acc3e9899ffc5bb3a75d8862a3d1fb5f
Arthur José <arthur.jose27@gmail.com>
2023-04-19 14:45:29 -0300

    Update tree.hs

--- Binary_Tree_Haskell/tree.hs
+++ Binary_Tree_Haskell/tree.hs
@@ -1,9 +1,20 @@
 data Arvore a = No a (Arvore a) (Arvore a) | Folha
-ins e Folha = No e Folha Folha
-ins e (No x esq dir) | e == x = No x esq dir
-                     | e < x = No x (ins e esq) dir 
-                     | otherwise = No x esq (ins e dir)
 
--- emOrdem :: Arvore a -> [a]
--- emOrdem Folha = 
--- emOrdem (No x esq dir) =
+ins :: Ord t => t -> Arvore t -> Arvore t
+ins e Folha = No e Folha Folha
+ins e (No x esq dir)
+  | e == x = No x esq dir
+  | e < x = No x (ins e esq) dir
+  | otherwise = No x esq (ins e dir)
+
+inOrder :: Arvore a -> [a]
+inOrder Folha = []
+inOrder (No x esq dir) = inOrder esq ++ [x] ++ inOrder dir
+
+preOrder :: Arvore a -> [a]
+preOrder Folha = []
+preOrder (No x esq dir) = [x] ++ preOrder esq ++ preOrder dir
+
+postOrder :: Arvore a -> [a]
+postOrder Folha = []
+postOrder (No x esq dir) = postOrder dir ++ postOrder esq ++ [x]
